241b44f8ab82d32882192df8db709ca1a2c7ddd5
pevgen <pevgen@gmail.com>
2025-07-19 09:20:53 +0300

    fixed ports, random service instance id

--- serviceA/src/main/java/ml/pevgen/demo/servicea/controller/ServiceAController.java
+++ serviceA/src/main/java/ml/pevgen/demo/servicea/controller/ServiceAController.java
@@ -1,14 +1,18 @@
 package ml.pevgen.demo.servicea.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceAController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/serviceA")
     public String serviceA() {
-        return "Service A";
+        return "Service A. ID: " + instanceId;
     }
 
 }
